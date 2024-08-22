7297bb2bdedb0356dbf5b5cace5e292a4e9405ec
Alexandroni07 <gaalexandroni@gmail.com>
2024-08-22 19:57:18 -0300

    insert implements

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,6 +32,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 
 	}
 
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -68,7 +68,7 @@ public class Seller implements Serializable {
 		this.baseSalary = baseSalary;
 	}
 
-	public Department getdepartment() {
+	public Department getDepartment() {
 		return department;
 	}
 
